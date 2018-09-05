3380264680a035dafc343300361540fed8ddfccb
statsbylopez <michael_lopez@alumni.brown.edu>
2018-09-05 18:24:53 -0400

    final tweaks

--- Labs/Lab0.R
+++ Labs/Lab0.R
@@ -1,2 +1,10 @@
 download.file("http://www.openintro.org/stat/data/mlb11.RData", destfile = "mlb11.RData")
 load("mlb11.RData")
+
+## Topics to review in R
+
+# What does a hashtag do?
+# How to store data?
+# How to view data?
+# Simple calculations
+# summary command
